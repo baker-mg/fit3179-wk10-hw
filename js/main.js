8029e16010e07a4ad3a70fa675508259da789876
Mathew <bakermg@outlook.com>
2021-10-10 19:01:22 +1100

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+vegaEmbed("#peaks-deaths", "peaks-deaths.vg.json", {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
+
+   vegaEmbed("#summit-date-climbers", "summit-date-climbers.vg.json", {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
